4490f42086fb57b7e81374e1404b3cb1907f767d
fduroni <fabrizio.duroni@gmail.com>
2021-01-13 08:59:25 +0100

    FieldsResolverByIteratingThroughThem migrated to kotlin :rocket:

--- minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/resolver/FieldsResolver.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/resolver/FieldsResolver.kt
@@ -3,5 +3,5 @@ package it.chicio.minesweeper.field.resolver
 import it.chicio.minesweeper.field.Field
 
 interface FieldsResolver {
-    fun resolve(fields: List<Field>): List<Field?>
+    fun resolve(fields: List<Field>): List<Field>
 }
--- minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/resolver/FieldsResolverByIteratingThroughThem.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/resolver/FieldsResolverByIteratingThroughThem.kt
@@ -1,14 +1,10 @@
 package it.chicio.minesweeper.field.resolver
 
 import it.chicio.minesweeper.field.Field
-import java.util.*
 
 class FieldsResolverByIteratingThroughThem(private val fieldResolver: FieldResolver) : FieldsResolver {
-    override fun resolve(fields: List<Field>): List<Field?> {
-        val resolvedFields = mutableListOf<Field>()
-        for (field in fields) {
-            resolvedFields.add(fieldResolver.resolve(field))
-        }
-        return resolvedFields.toList()
-    }
+    override fun resolve(fields: List<Field>): List<Field> =
+            fields
+                    .map { fieldResolver.resolve(it) }
+                    .toList()
 }
