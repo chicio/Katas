f747e5ca214852cc61dca4284592704d21cdc106
fduroni <fabrizio.duroni@gmail.com>
2021-01-14 19:26:08 +0100

    Removed JvmField annotation :rocket:

--- minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/parser/FieldsParsingStatus.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/parser/FieldsParsingStatus.kt
@@ -4,12 +4,9 @@ import it.chicio.minesweeper.field.Field
 import java.util.*
 
 data class FieldsParsingStatus(
-    @kotlin.jvm.JvmField
     var currentField: Field?,
-    @kotlin.jvm.JvmField
     var currentRowContent: String?,
     var currentRow: Int,
     var headerNumberOfRowsForCurrentField: Int,
-    @kotlin.jvm.JvmField
     var fieldsParsed: ArrayList<Field>?
 )
