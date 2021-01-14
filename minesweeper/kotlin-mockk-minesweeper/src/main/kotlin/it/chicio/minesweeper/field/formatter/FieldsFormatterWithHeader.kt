36a75a560503f9a3122046eb811c48883a7f8420
fduroni <fabrizio.duroni@gmail.com>
2021-01-14 09:16:29 +0100

    Improved FieldsFormatterWithHeader :rocket:

--- minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/formatter/FieldsFormatterWithHeader.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/main/kotlin/it/chicio/minesweeper/field/formatter/FieldsFormatterWithHeader.kt
@@ -3,22 +3,9 @@ package it.chicio.minesweeper.field.formatter
 import it.chicio.minesweeper.field.Field
 
 class FieldsFormatterWithHeader(private val fieldFormatter: FieldFormatter) : FieldsFormatter {
-    override fun format(fields: List<Field>): String {
-        val formattedField = StringBuilder()
-        var currentFieldPosition = 1
-        for (field in fields) {
-            formattedField.append(createHeaderFor(currentFieldPosition)).append(fieldFormatter.format(field))
-            appendNewlineIfNeeded(fields, formattedField, currentFieldPosition)
-            currentFieldPosition++
-        }
-        return formattedField.toString()
-    }
-
-    private fun appendNewlineIfNeeded(fields: List<Field?>, formattedField: StringBuilder, currentFieldPosition: Int) {
-        if (currentFieldPosition != fields.size) {
-            formattedField.append(System.getProperty("line.separator"))
-        }
-    }
+    override fun format(fields: List<Field>): String = fields
+            .mapIndexed { index, field -> createHeaderFor(index + 1).plus(fieldFormatter.format(field)) }
+            .joinToString(System.getProperty("line.separator"))
 
     private fun createHeaderFor(currentField: Int): String =
             "field #" + currentField + ":" + System.getProperty("line.separator")
