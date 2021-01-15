4b8d11e89836a72c40ea85832d09b5c1b13c1bf1
fduroni <fabrizio.duroni@gmail.com>
2021-01-15 22:14:30 +0100

    Continuing migration :rocket:

--- minesweeper/kotlin-mockk-minesweeper/src/test/kotlin/it/chicio/minesweeper/field/resolver/FieldResolverByIteratingThroughEachValueTest.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/test/kotlin/it/chicio/minesweeper/field/resolver/FieldResolverByIteratingThroughEachValueTest.kt
@@ -8,7 +8,7 @@ import org.junit.Before
 import org.junit.Test
 
 class FieldResolverByIteratingThroughEachValueTest {
-    private var fieldResolverByIteratingThroughEachValue: FieldResolverByIteratingThroughEachValue? = null
+    private lateinit var fieldResolverByIteratingThroughEachValue: FieldResolverByIteratingThroughEachValue
     @Before
     fun setUp() {
         fieldResolverByIteratingThroughEachValue = FieldResolverByIteratingThroughEachValue()
@@ -16,49 +16,49 @@ class FieldResolverByIteratingThroughEachValueTest {
 
     @Test
     fun resolve1x1FieldWithABomb() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf("*"))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*"))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*"))))))
     }
 
     @Test
     fun resolve1x2FieldWithABombOnTheLeft() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf("*", "."))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "."))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "1"))))))
     }
 
     @Test
     fun resolve1x2FieldWithABombOnTheRight() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("1", "*"))))))
     }
 
     @Test
     fun resolve2x2FieldWith3Bomb() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"), arrayOf("*", "*"))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"), arrayOf("*", "*"))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("3", "*"), arrayOf("*", "*"))))))
     }
 
     @Test
     fun resolve2x3FieldWith3Bomb() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", "*"), arrayOf("*", "*", "*"))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", "*"), arrayOf("*", "*", "*"))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "5", "*"), arrayOf("*", "*", "*"))))))
     }
 
     @Test
     fun resolve3x3FieldWith3Bomb() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", ".", "*"), arrayOf("*", "*", "*"))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", ".", "*"), arrayOf("*", "*", "*"))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", "8", "*"), arrayOf("*", "*", "*"))))))
     }
 
     @Test
     fun resolveTestField1() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", ".", ".", "."), arrayOf(".", ".", ".", ".", "."), arrayOf(".", "*", ".", ".", "."))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", ".", ".", "."), arrayOf(".", ".", ".", ".", "."), arrayOf(".", "*", ".", ".", "."))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "*", "1", "0", "0"), arrayOf("3", "3", "2", "0", "0"), arrayOf("1", "*", "1", "0", "0"))))))
     }
 
     @Test
     fun resolveTestField2() {
-        val field = fieldResolverByIteratingThroughEachValue!!.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", ".", "."), arrayOf(".", ".", ".", "."), arrayOf(".", "*", ".", "."), arrayOf(".", ".", ".", "."))))
+        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", ".", "."), arrayOf(".", ".", ".", "."), arrayOf(".", "*", ".", "."), arrayOf(".", ".", ".", "."))))
         Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "1", "0", "0"), arrayOf("2", "2", "1", "0"), arrayOf("1", "*", "1", "0"), arrayOf("1", "1", "1", "0"))))))
     }
 }
--- minesweeper/kotlin-mockk-minesweeper/src/test/kotlin/it/chicio/minesweeper/field/resolver/FieldsResolverByIteratingThroughThemTest.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/test/kotlin/it/chicio/minesweeper/field/resolver/FieldsResolverByIteratingThroughThemTest.kt
@@ -1,34 +1,67 @@
 package it.chicio.minesweeper.field.resolver
 
 import it.chicio.minesweeper.FieldFactory
-import it.chicio.minesweeper.field.Field
-import org.hamcrest.CoreMatchers
-import org.junit.Assert
-import org.junit.Test
-import org.mockito.Mockito
-import java.util.*
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.mockito.Mockito.`when`
+import org.mockito.Mockito.mock
 
+@DisplayName("FieldsResolverByIteratingThroughThem")
 class FieldsResolverByIteratingThroughThemTest {
-    private val field = FieldFactory().make(arrayOf(arrayOf("*", "*", ".", ".", "."), arrayOf(".", ".", ".", ".", "."), arrayOf(".", "*", ".", ".", ".")))
-    private val resolvedField = FieldFactory().make(arrayOf(arrayOf("*", "*", "1", "0", "0"), arrayOf("3", "3", "2", "0", "0"), arrayOf("1", "*", "1", "0", "0")))
-    private val anotherField = FieldFactory().make(arrayOf(arrayOf("*", ".", ".", "."), arrayOf(".", ".", ".", "."), arrayOf(".", "*", ".", "."), arrayOf(".", ".", ".", ".")))
-    private val anotherResolvedField = FieldFactory().make(arrayOf(arrayOf("*", "*", "1", "0", "0"), arrayOf("3", "3", "2", "0", "0"), arrayOf("1", "*", "1", "0", "0")))
-    @Test
-    fun resolveSingleFieldList() {
-        val fieldResolver = Mockito.mock(FieldResolver::class.java)
-        Mockito.`when`(fieldResolver.resolve(field)).thenReturn(resolvedField)
-        val fieldsResolverByIteratingThroughThem = FieldsResolverByIteratingThroughThem(fieldResolver)
-        val fields = fieldsResolverByIteratingThroughThem.resolve(listOf(field))
-        Assert.assertThat(fields, CoreMatchers.`is`(listOf(resolvedField)))
+    @Nested
+    @DisplayName("resolve")
+    inner class Resolve {
+        @Test
+        fun `single field list`() {
+            val fieldResolver = mock(FieldResolver::class.java)
+            `when`(fieldResolver.resolve(field)).thenReturn(resolvedField)
+            val fieldsResolverByIteratingThroughThem = FieldsResolverByIteratingThroughThem(fieldResolver)
+            val fields = fieldsResolverByIteratingThroughThem.resolve(listOf(field))
+            assertEquals(fields, listOf(resolvedField))
+        }
+
+        @Test
+        fun `fields list`() {
+            val fieldResolver = mock(FieldResolver::class.java)
+            `when`(fieldResolver.resolve(field)).thenReturn(resolvedField)
+            `when`(fieldResolver.resolve(anotherField)).thenReturn(anotherResolvedField)
+            val fieldsResolverByIteratingThroughThem = FieldsResolverByIteratingThroughThem(fieldResolver)
+            val fields = fieldsResolverByIteratingThroughThem.resolve(listOf(field, anotherField))
+            assertEquals(fields, listOf(resolvedField, anotherResolvedField))
+        }
     }
 
-    @Test
-    fun resolveFieldsList() {
-        val fieldResolver = Mockito.mock(FieldResolver::class.java)
-        Mockito.`when`(fieldResolver.resolve(field)).thenReturn(resolvedField)
-        Mockito.`when`(fieldResolver.resolve(anotherField)).thenReturn(anotherResolvedField)
-        val fieldsResolverByIteratingThroughThem = FieldsResolverByIteratingThroughThem(fieldResolver)
-        val fields = fieldsResolverByIteratingThroughThem.resolve(Arrays.asList(field, anotherField))
-        Assert.assertThat(fields, CoreMatchers.`is`(Arrays.asList(resolvedField, anotherResolvedField)))
+    companion object {
+        private val field = FieldFactory().make(
+                arrayOf(
+                        arrayOf("*", "*", ".", ".", "."),
+                        arrayOf(".", ".", ".", ".", "."),
+                        arrayOf(".", "*", ".", ".", ".")
+                )
+        )
+        private val resolvedField = FieldFactory().make(
+                arrayOf(
+                        arrayOf("*", "*", "1", "0", "0"),
+                        arrayOf("3", "3", "2", "0", "0"),
+                        arrayOf("1", "*", "1", "0", "0")
+                )
+        )
+        private val anotherField = FieldFactory().make(
+                arrayOf(
+                        arrayOf("*", ".", ".", "."),
+                        arrayOf(".", ".", ".", "."),
+                        arrayOf(".", "*", ".", "."),
+                        arrayOf(".", ".", ".", ".")
+                )
+        )
+        private val anotherResolvedField = FieldFactory().make(
+                arrayOf(
+                        arrayOf("*", "*", "1", "0", "0"),
+                        arrayOf("3", "3", "2", "0", "0"),
+                        arrayOf("1", "*", "1", "0", "0")
+                )
+        )
     }
 }
