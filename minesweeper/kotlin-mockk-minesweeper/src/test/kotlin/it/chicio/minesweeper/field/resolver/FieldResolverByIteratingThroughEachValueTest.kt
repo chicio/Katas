78338350690c7f28c63cd85cc4f6c6209ae5e1a8
fduroni <fabrizio.duroni@gmail.com>
2021-01-16 08:53:00 +0100

    Improved FieldResolverByIteratingThroughEachValueTest with junit 5 :rocket:

--- minesweeper/kotlin-mockk-minesweeper/src/test/kotlin/it/chicio/minesweeper/field/resolver/FieldResolverByIteratingThroughEachValueTest.kt
+++ minesweeper/kotlin-mockk-minesweeper/src/test/kotlin/it/chicio/minesweeper/field/resolver/FieldResolverByIteratingThroughEachValueTest.kt
@@ -1,64 +1,88 @@
 package it.chicio.minesweeper.field.resolver
 
 import it.chicio.minesweeper.FieldFactory
-import it.chicio.minesweeper.field.Field
-import org.hamcrest.CoreMatchers
-import org.junit.Assert
-import org.junit.Before
-import org.junit.Test
 
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+
+@DisplayName("FieldResolverByIteratingThroughEachValue")
 class FieldResolverByIteratingThroughEachValueTest {
     private lateinit var fieldResolverByIteratingThroughEachValue: FieldResolverByIteratingThroughEachValue
-    @Before
+
+    @BeforeEach
     fun setUp() {
         fieldResolverByIteratingThroughEachValue = FieldResolverByIteratingThroughEachValue()
     }
 
-    @Test
-    fun resolve1x1FieldWithABomb() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*"))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*"))))))
-    }
+    @Nested
+    @DisplayName("resolve")
+    inner class Resolve {
+        @Test
+        fun `1x1 field with a bomb`() =
+                assertEquals(
+                        fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*")))),
+                        FieldFactory().make(arrayOf(arrayOf("*")))
+                )
 
-    @Test
-    fun resolve1x2FieldWithABombOnTheLeft() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "."))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "1"))))))
-    }
+        @Nested
+        @DisplayName("1x2 field")
+        inner class Field1x2 {
+            @Test
+            fun `with a bomb on the left`() =
+                    assertEquals(
+                            fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".")))),
+                            FieldFactory().make(arrayOf(arrayOf("*", "1")))
+                    )
 
-    @Test
-    fun resolve1x2FieldWithABombOnTheRight() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("1", "*"))))))
-    }
+            @Test
+            fun `with a bomb on the right`() =
+                    assertEquals(
+                            fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*")))),
+                            FieldFactory().make(arrayOf(arrayOf("1", "*"))
+                            )
+                    )
+        }
 
-    @Test
-    fun resolve2x2FieldWith3Bomb() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"), arrayOf("*", "*"))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("3", "*"), arrayOf("*", "*"))))))
-    }
+        @Test
+        fun `2x2 field with 3 bomb`() =
+                assertEquals(
+                        fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf(".", "*"), arrayOf("*", "*")))),
+                        FieldFactory().make(arrayOf(arrayOf("3", "*"), arrayOf("*", "*")))
+                )
 
-    @Test
-    fun resolve2x3FieldWith3Bomb() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", "*"), arrayOf("*", "*", "*"))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "5", "*"), arrayOf("*", "*", "*"))))))
-    }
+        @Test
+        fun `2x3 field with 3 bomb`() =
+                assertEquals(
+                        fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", "*"), arrayOf("*", "*", "*")))),
+                        FieldFactory().make(arrayOf(arrayOf("*", "5", "*"), arrayOf("*", "*", "*")))
+                )
 
-    @Test
-    fun resolve3x3FieldWith3Bomb() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", ".", "*"), arrayOf("*", "*", "*"))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", "8", "*"), arrayOf("*", "*", "*"))))))
-    }
+        @Test
+        fun `3x3 field with 3 bomb`() =
+                assertEquals(
+                        fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", ".", "*"), arrayOf("*", "*", "*")))),
+                        FieldFactory().make(arrayOf(arrayOf("*", "*", "*"), arrayOf("*", "8", "*"), arrayOf("*", "*", "*")))
+                )
 
-    @Test
-    fun resolveTestField1() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", ".", ".", "."), arrayOf(".", ".", ".", ".", "."), arrayOf(".", "*", ".", ".", "."))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "*", "1", "0", "0"), arrayOf("3", "3", "2", "0", "0"), arrayOf("1", "*", "1", "0", "0"))))))
-    }
+        @Nested
+        @DisplayName("a test field")
+        inner class TestField {
+            @Test
+            fun `3x5`() =
+                    assertEquals(
+                            fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", "*", ".", ".", "."), arrayOf(".", ".", ".", ".", "."), arrayOf(".", "*", ".", ".", ".")))),
+                            FieldFactory().make(arrayOf(arrayOf("*", "*", "1", "0", "0"), arrayOf("3", "3", "2", "0", "0"), arrayOf("1", "*", "1", "0", "0")))
+                    )
 
-    @Test
-    fun resolveTestField2() {
-        val field = fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", ".", "."), arrayOf(".", ".", ".", "."), arrayOf(".", "*", ".", "."), arrayOf(".", ".", ".", "."))))
-        Assert.assertThat(field, CoreMatchers.`is`(CoreMatchers.equalTo(FieldFactory().make(arrayOf(arrayOf("*", "1", "0", "0"), arrayOf("2", "2", "1", "0"), arrayOf("1", "*", "1", "0"), arrayOf("1", "1", "1", "0"))))))
+            @Test
+            fun `4x4`() =
+                    assertEquals(
+                            fieldResolverByIteratingThroughEachValue.resolve(FieldFactory().make(arrayOf(arrayOf("*", ".", ".", "."), arrayOf(".", ".", ".", "."), arrayOf(".", "*", ".", "."), arrayOf(".", ".", ".", ".")))),
+                            FieldFactory().make(arrayOf(arrayOf("*", "1", "0", "0"), arrayOf("2", "2", "1", "0"), arrayOf("1", "*", "1", "0"), arrayOf("1", "1", "1", "0")))
+                    )
+        }
     }
 }
