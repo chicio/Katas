689d920d3329625f65d88bac61c7236ac0efc7a4
Fabrizio Duroni <fabrizio.duroni@lastminute.com>
2023-05-20 14:48:17 +0200

    Start birthday greetings kata :fire:

---
+++ birthday-greetings/swift-birthday-greetings/swift-birthday-greetings/swift_birthday_greetingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_birthday_greetingsApp.swift
+//  swift-birthday-greetings
+//
+//  Created by Fabrizio Duroni on 20/05/23.
+//
+
+import SwiftUI
+
+@main
+struct swift_birthday_greetingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
